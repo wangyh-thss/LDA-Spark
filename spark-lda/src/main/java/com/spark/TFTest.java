0a57b7b54cf791e35ceef5e136be484347ed3158
xzr12 <xzr12thss@gmail.com>
2016-12-29 02:03:40 +0800

    change args and part of feature extraction

--- spark-lda/src/main/java/com/spark/TFTest.java
+++ spark-lda/src/main/java/com/spark/TFTest.java
@@ -1,10 +1,7 @@
 package com.spark; /**
  * Created by wangyihan on 2016/12/19.
  */
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -12,12 +9,15 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.mllib.clustering.DistributedLDAModel;
 import org.apache.spark.mllib.clustering.LDA;
-import org.apache.spark.mllib.linalg.Matrix;
-import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.feature.HashingTF;
+import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.rdd.RDD;
 import scala.Tuple2;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,11 +26,12 @@ public class TFTest {
         String inputFile = args[0];
         String outputFile = args[1];
         String modelDir = args[2];
+        int topicNum = Integer.parseInt(args[3]);
         SparkConf conf = new SparkConf().setAppName("datapreTest");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-
-        JavaRDD<List<String>> documents = sc.textFile(inputFile).map(
+        JavaRDD<String> aa = sc.textFile(inputFile).cache();
+        JavaRDD<List<String>> documents = aa.map(
                 new Function<String, List<String>>() {
                     public List<String> call(String s) {
                         String sentences = s.split("\t\n")[0].split("\2")[18];
@@ -53,16 +54,39 @@ public class TFTest {
         ));
         corpus.cache();
 
-        int topicNum = 10;
+//        int topicNum = 10;
         LDA lda = new LDA().setK(topicNum);
         DistributedLDAModel ldaModel = (DistributedLDAModel)lda.run(corpus);
 
         RDD<Tuple2<Object, Vector>> topicDist = ldaModel.topicDistributions();
         Tuple2[] topicDistList = (Tuple2[])topicDist.collect();
+
         for (Object el: topicDistList) {
             Tuple2 tel = (Tuple2) el;
-            System.out.println(tel._2);
+            String stel = tel._2().toString();
+            String[] s = stel.substring(1, stel.length() - 2).split(",");
+            for (int j = 0; j < s.length; j++) {
+                System.out.println(Double.parseDouble(s[j]));
+            }
         }
+
+        JavaRDD<Tuple2<Object, Vector>> temp = topicDist.toJavaRDD();
+        for(Tuple2<Object, Vector> t: temp.collect()) {
+            System.out.println(t);
+        }
+//        temp.saveAsTextFile(outputFile);
+
+//        JavaRDD<String> topicDistList = JavaRDD.fromRDD(topicDist, topicDist.elementClassTag()).map(
+//                new Function<Tuple2<Object, Vector>, String>() {
+//                    public String call(Tuple2<Object, Vector> topic) {
+//                        System.out.println(topic._1());
+//                        System.out.println(topic._2());
+//                        return "";
+//                    }
+//                }
+//        );
+
+
 
 
 //        System.out.println("Learned topics (as distributions over vocab of " + ldaModel.vocabSize() + " words):");
